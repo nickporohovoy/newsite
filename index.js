18b240ab20bf10eae5a3316a796e8b6c509ae2c9
nickporohovoy <cosmosvnutri8@mail.ru>
2024-12-23 09:01:46 +0400

    Update server to serve templates

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Укажите путь к папке с шаблонами
+const templatesPath = path.join(__dirname, 'recipes', 'templates', 'recipes');
+
+// Настройка статической папки для обслуживания файлов (CSS, JS, изображения, если есть)
+app.use(express.static(templatesPath));
+
+// Главная страница
 app.get('/', (req, res) => {
-    res.send('Hello, Render!');
+    res.sendFile(path.join(templatesPath, 'home.html'));
 });
 
+// Страница добавления рецепта
+app.get('/add-recipe', (req, res) => {
+    res.sendFile(path.join(templatesPath, 'add_recipe.html'));
+});
+
+// Детализация рецепта
+app.get('/recipe-detail', (req, res) => {
+    res.sendFile(path.join(templatesPath, 'recipe_detail.html'));
+});
+
+// Запуск сервера
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
